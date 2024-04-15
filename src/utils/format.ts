d4f0f3efcc205547a81bde2c2da72efdc0d842e1
Zhongwei Li <lizhongwei.nkcs@gmail.com>
2024-04-15 22:23:30 +0800

    Update footer links and date formatting in codebase
    - Update translation for "inspired-by" in footer section
    - Update link in footer to point to `github.com/yuhangch/astro-theme-panda`
    - Change date formatting in `formatDateMD` to include day
    - Change date formatting in `formatDateYMD` to include day and full year

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -15,18 +15,18 @@ export function formatDateMD(dateStr: string, locale: string = 'zh') {
     const date = DateTime.fromISO(dateStr)
     locale = defaultLocale // force to default language
     if (locale === 'en') {
-        return date.setLocale(locale).toFormat('MMM')
+        return date.setLocale(locale).toFormat('dd, MMM')
     }
-    return date.toFormat('M月')
+    return date.toFormat('M月d日')
 }
 
 export function formatDateYMD(dateString: string, locale: string = 'zh') {
     locale = defaultLocale // force to default language
     const date = DateTime.fromISO(dateString)
     if (locale === 'en') {
-        return date.setLocale(locale).toFormat('MMM, yyyy')
+        return date.setLocale(locale).toFormat('dd, MMM, yyyy')
     }
-    return date.toFormat('yyyy-MM')
+    return date.toFormat('yyyy-MM-dd')
 }
 
 export function slugifySpace(old: string) {
